05e1048c1048d7103b7b59a5c4f2e88f8396a15f
je-pbx <johannes.erber@photobox.com>
2015-12-03 16:43:06 +0100

    Remove history-api-fallback and default options

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const express = require('express')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
-// const historyApiFallback = require('connect-history-api-fallback')
 const chalk = require('chalk')
 const webpackConfig = require('./build/webpack/development_hot')
 
@@ -15,20 +14,9 @@ const server = express()
 const host = 'localhost'
 const port = 3000
 
-// server.use(historyApiFallback({
-//   verbose: false
-// }))
-
 server.use(webpackDevMiddleware(compiler, {
   publicPath: webpackConfig.output.publicPath,
-  contentBase: 'src',
-  hot: true,
-  quiet: false,
-  noInfo: false,
-  lazy: false,
-  stats: {
-    colors: true
-  }
+  contentBase: 'src'
 }))
 
 server.use(webpackHotMiddleware(compiler))
