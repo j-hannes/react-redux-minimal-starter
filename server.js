ac078bcb40c1ada50584db0b747d89f17f1a52be
je-pbx <johannes.erber@photobox.com>
2015-12-03 16:15:45 +0100

    Move server to root

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
+/* eslint no-console:0 */
 require('babel/register')
 
 const chalk = require('chalk')
-const server = require('../server/app')
+const server = require('./server/app')
 
 const host = 'localhost'
 const port = 3000
 
 server.listen(port, host, function() {
-  /* eslint no-console:0 */
   console.log(chalk.green(
     `Server is now running at ${host}:${port}.`
   ))
