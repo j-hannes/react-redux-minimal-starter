0cec50e2a35de03669596a345feb522488ef283a
je-pbx <johannes.erber@photobox.com>
2015-12-04 15:25:52 +0100

    Reenable hmr transform

--- webpack.config.js
+++ webpack.config.js
@@ -66,10 +66,16 @@ const webpackConfig = {
           plugins: ['react-transform'],
           extra: {
             'react-transform': {
-              transforms: [{
-                transform: 'react-transform-catch-errors',
-                imports: ['react', 'redbox-react']
-              }]
+              transforms: [
+                {
+                  transform: 'react-transform-catch-errors',
+                  imports: ['react', 'redbox-react']
+                }, {
+                  transform: 'react-transform-hmr',
+                  imports: ['react'],
+                  locals: ['module']
+                }
+              ]
             }
           }
         }
